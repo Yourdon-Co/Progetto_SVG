aec67766d47f8cfa79914e266e785e6955e31b8e
Daniele Cecca <d.cecca1@studenti.uniba.it>
2022-05-26 12:29:42 +0200

    Realizzato sistema di spawning (script: Spawner)

--- My project/Assets/Scripts/Gravity.cs
+++ My project/Assets/Scripts/Gravity.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerGravity : MonoBehaviour
+public class Gravity : MonoBehaviour
 
 {
     public PlanetScript attractorPlanet;
